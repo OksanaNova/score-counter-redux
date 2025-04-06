b6f0bb63328f64cdd465bf4e7ac7c9023399dc77
OksanaNova <o.nova0804@gmail.com>
2025-04-05 19:03:21 -0600

    counter, store added

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counter from './counter';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counter
+    },
 })
